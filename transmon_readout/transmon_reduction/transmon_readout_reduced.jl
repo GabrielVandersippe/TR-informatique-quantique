77a5889a36bd6a976fd16d1b97372e765ce68ec4
Gabriel Vandersippe <gabriel.vandersippe@etu.minesparis.psl.eu>
2026-01-12 14:46:47 +0100

    Added a plot

--- transmon_readout/transmon_reduction/transmon_readout_reduced.jl
+++ transmon_readout/transmon_reduction/transmon_readout_reduced.jl
@@ -90,3 +90,62 @@ function states_dmrg(ECR, ECoup, EL; nb_states = 4, resonator_trunc=40) #Transmo
 
 
 end
+
+
+
+# --- Parameters ---
+nb_states = 6
+
+# --- Sweep over EL to vary omega_r ---
+EL_vals = range(0.1, 5, length=20)
+Omega_r = sqrt.(8 * ECR .* EL_vals)
+
+krylov_data = [Float64[] for _ in 1:nb_states]
+dmrg_data = [Float64[] for _ in 1:nb_states]
+
+
+# ---- Computiing the energies ----
+for el in EL_vals
+    # Krylov Calculation
+    H_k = hamiltonian_tr(ECT, ECR, ECoup, EJ, el)
+    vals_k, _, _ = eigsolve(H_k, nb_states, :SR)
+    vals_k .-= vals_k[1] 
+    
+    # DMRG Calculation
+    vals_d = states_dmrg(ECR, ECoup, el; nb_states=nb_states)
+    vals_d .-= vals_d[1] 
+
+    for i in 1:nb_states
+        push!(krylov_data[i], vals_k[i])
+        push!(dmrg_data[i], vals_d[i])
+    end
+    print(".")
+end
+
+
+
+# ---- Plotting with CairoMakie ------
+fig = Figure(resolution = (800, 600), font = "DejaVu Sans")
+ax = Axis(fig[1, 1], 
+    xlabel = L"\omega_r(E_L) = \sqrt{8 E_{C_R} E_L} [GHz]", 
+    ylabel = L"E_n - E_0 [GHz]",
+    title = "Transmon-Resonator Energy Levels")
+
+colors = Makie.wong_colors()
+
+for i in 1:nb_states
+    lines!(ax, Omega_r, krylov_data[i], 
+        linestyle = :dash, 
+        color = (colors[i], 0.5), 
+        linewidth = 2,
+        label = i == 1 ? "Krylov" : nothing)
+    
+    lines!(ax, Omega_r, dmrg_data[i], 
+        linestyle = :solid, 
+        color = (colors[i], 0.5), 
+        linewidth = 2,
+        label = i == 1 ? "DMRG" : nothing)
+end
+
+axislegend(ax, position = :lt)
+save("./transmon_readout/fock_basis/energies_vs_omega_r.png", fig)
